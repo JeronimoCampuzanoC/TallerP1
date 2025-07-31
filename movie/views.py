3e9509f9affc2a17cd8f701e069b015fa856cd1a
Jeronimo Campuzano Castaño <jcampuzanc@eafit.edu.co>
2025-07-31 11:35:20 -0500

    nuevo archivo about.hmtl

--- movie/views.py
+++ movie/views.py
@@ -5,4 +5,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'Jeronimo Campuzano'})
 
 def about(request):
-    return HttpResponse('<h1>About Movie Reviews</h1>')
+    return render(request, 'about.html')
