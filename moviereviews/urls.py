a492a9a09923581d8caaec9474aface68f1e9cad
Jeronimo Campuzano Castaño <jcampuzanc@eafit.edu.co>
2025-08-24 15:10:55 -0500

    navbar

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
+    path('', movieViews.home, name='home'                                                                                                                               ),
     path('about/', movieViews.about),
     path('news/', include('news.urls')),
 ]
