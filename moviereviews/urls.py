5d00c43745666cb33c33b256f6a24085fd86c816
Jeronimo Campuzano Castaño <jcampuzanc@eafit.edu.co>
2025-08-24 22:33:24 -0500

    statistics year

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', movieViews.home, name='home'                                                                                                                               ),
     path('about/', movieViews.about),
     path('news/', include('news.urls')),
+    path('statistics/', movieViews.statistics_view, name='statistics'),
 ]
 
 
